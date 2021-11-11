a83f969d8eb83c52e6d91dc0f4170fabf69aa151
Kweku <kwekuappiah11@gmail.com>
2021-11-11 21:54:31 +0000

    header and footer wireframes made; shopping_cart wireframe

---
+++ header.php
@@ -0,0 +1,49 @@
+<link rel="stylesheet" href="styles/header.css">
+<link rel="stylesheet" href="styles/main.css">
+<link rel="stylesheet" href="styles/footer.css">
+<!-- boot strap icons  -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+	<header class=" cs-primary">
+		<nav class="navbar ">
+			<div class="container-fluid">
+				<a class="navbar-brand link" href="codeshop.php">CodeShop</a>
+				<a href="marketplace.php" class="link">Marketplace</a>
+
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+
+
+				<!-- search field -->
+				<input type="search" name="searchField" id="search_field">
+
+
+
+				<!-- Links to the left of header -->
+				<a href="marketplace.php" class="link">
+				<div class="link">
+					<i class="bi bi-person-circle"></i>
+
+					Account
+				</div></a>
+
+				<a href="marketplace.php" class="link">
+				<div class="link">
+					<i class="bi bi-question"></i>
+					Help
+				</div></a>
+
+				<a href="shopping_cart.php" class="link">
+				<div class="link">
+					<i class="bi bi-cart"></i>
+					Cart
+				</div></a>
+
+
+			</div>
+		</nav>
+	</header>
---
+++ shopping_cart.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/shopping_cart.css">
+  <title>CodeShop - Your Cart</title>
+  <?php require_once "header.php"; ?>
+
+
+  <div class="d-flex justify-content-center">
+
+
+    <div class="card col-8">
+      <div class="row ">
+        <div class="col-10 " id="cart-heading-div">
+
+          <h4 class="text-uppercase inline" id="cart-heading">your shopping cart</h4>
+        </div>
+
+        <!-- price section -->
+        <div class="price col-1">
+          GHC <br>
+          <strong>250</strong>
+        </div>
+
+
+
+      </div>
+      <table class="table ">
+        <colgroup>
+          <col class="col-md-1">
+          <col class="col-md-7 ">
+          <col class="col-md-2 cs-tertiary">
+          <col class="col-md-2 cs-tertiary">
+        </colgroup>
+        <thead>
+
+          <tr>
+            <th scope="col"></th>
+            <th scope="col" class="text-uppercase">Item</th>
+            <th class="text-uppercase" scope="col">unit price</th>
+            <th class="text-uppercase" scope="col">sub total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <!-- remove action -->
+            <th scope="row">
+
+            <h2 class="icon-heading"><i class="bi bi-x-square"></i></h2>
+            </th>
+
+            <!-- Product information  -->
+            <td>
+
+            <div class="row">
+            <div class="col-2 product-image m-2 justify-content-center">
+              <img src="https://picsum.photos/200/300" class="product-image" alt="">
+            </div>
+            <div class="col">
+
+              <strong>Inventory application built with flutter</strong>
+              <br>
+              By Collins Dauda
+              <br>
+              <h2 class="icon-heading"><i class="bi bi-bookmark-heart"></i></h2>
+
+            </div>
+            </div>
+
+
+            </td>
+
+            <!-- unit price  -->
+            <td>
+              <!-- price section -->
+              <div class="price  cs-background">
+                GHC <br>
+                <strong>250</strong>
+              </div>
+            </td>
+
+
+            <!-- sub total  -->
+            <td>
+              <!-- price section -->
+              <div class="price cs-tertiary">
+                GHC
+                <br>
+                <strong>250</strong>
+              </div>
+            </td>
+
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="row justify-content-end">
+        <a href="marketplace.php" class="btn btn-info a-button" role="button">Continue shopping</a>
+        <a href="" class="btn btn-info a-button" role="button">Proceed to Check out</a>
+
+      </div>
+
+    </div>
+
+
+
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <?php require_once "footer.php"; ?>
+  </body>
+
+</html>
