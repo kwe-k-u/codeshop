9b17c2864d30fd493754f56d3ce12918a19831b6
Yemofio <95033608+Yemofio@users.noreply.github.com>
2021-11-25 22:29:20 +0000

    Update add_to_cart.php

--- backend/add_to_cart.php
+++ backend/add_to_cart.php
@@ -5,16 +5,14 @@
 <?php 
 session_start();
 
-require('../Controllers/cart_controller.php');
+require('cart_controller.php');
 
 
-// check if theres a POST variable with the name 'add_pname'
 if(isset($_POST['add_cart'])){
-    // retrieve the name, description and quantity from the form submission
     $code_id = $_POST['cd_id'];
     $user_id = $_POST['us_id'];
     $quantity = 1;
-    // call the add_product_controller function: return true or false
+ 
     $check_dup= check_duplicate_controller($code_id,$user_id);
     
 
@@ -25,11 +23,9 @@ if(isset($_POST['add_cart'])){
         echo 'setTimeout(function () { swal(" Already In  Cart","Message!","failure");';
         echo '}, 1000);</script>';
       
-        // header('Location: ');
     } 
     else {
         $result =  add_to_cart_controller($code_id, $user_id,$quantity);
-        // header('Location: ../View/add_code_page.php');
         echo '<script type="text/javascript">';
         echo 'setTimeout(function () { swal(" Added to cart","Message!","success");';
         echo '}, 1000);</script>';
@@ -39,22 +35,17 @@ if(isset($_POST['add_cart'])){
 }
 
 if(isset($_POST['del_cart'])){
-    // retrieve the name, description and quantity from the form submission
     $code_id = $_POST['cd_id'];
     $user_id = $_POST['us_id'];
     $quantity = 1;
-    // call the add_product_controller function: return true or false
     
     $result =  delete_from_cart_controller($code_id);
 
-
-
     if($result){
-         header('Location: ../views/cart.php ');
+         header('Location: cart.php ');
     } 
     else {
        
-        // header('Location: ../View/add_code_page.php');
         echo '<script type="text/javascript">';
         echo 'setTimeout(function () { swal(" Unable to delete","Message!","success");';
         echo '}, 1000);</script>';
