411fd2e3d545429a125dcb7345b5d4bfae6953b6
Yemofio <95033608+Yemofio@users.noreply.github.com>
2021-11-25 22:33:49 +0000

    Update cart_class.php

--- backend/cart_class.php
+++ backend/cart_class.php
@@ -1,34 +1,28 @@
 <?php  
 
-require ('../Settings/db_class.php');
+require ('Database_class.php');
 
 class Cart extends Connection{
 
     function add_to_cart($cd_id,$us_id,$quantity){
-		// return true or false
 		return $this->query("insert into cart(cd_id,us_id,quantity) values('$cd_id', '$us_id','$quantity')");
 	}
 
 	function view_code($cd_id)
     {
-        // return array or false
         return $this->fetch("select * from cart inner join code on cart.cd_id = code.code_id where cd_id='$cd_id'");
     }
 
     function delete_from_cart($id){
-		// return true or false
 		return $this->query("delete from cart where code_id = '$id'");
 	}
 
   function update_code_quantity($id,$cd_id,$quantity){
-		// return true or false
 		return $this->query("update cart set quantity='$quantity' where cd_id = '$id' and cd_id = '$cd_id'");
 	}
 
 	function sum_price($us_id){
-		// return true or false
 		return $this->fetch("select SUM(code_price) as total from cart inner join code on cart.cd_id = cd.cd_id where us_id='$us_id'");
-		// return $this->query("Select SUM() as total from cart where us_id = '$us_id'");
 	}
 
 	function check_duplicate($code_id,$user_id){
