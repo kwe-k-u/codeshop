c60d3129b440cb6b7160e8191bb3331ac4c77e20
Kweku <kwekuappiah11@gmail.com>
2021-11-25 13:26:43 +0000

    Added payment flow

--- shopping_cart.php
+++ shopping_cart.php
@@ -32,6 +32,8 @@
 
 
       </div>
+
+      <!-- cart content  -->
       <table class="table ">
         <colgroup>
           <col class="col-md-1">
@@ -51,28 +53,31 @@
         <tbody>
           <tr>
             <!-- remove action -->
-            <th scope="row" >
+            <th scope="row">
 
-            <h2 class="icon-heading"><i class="bi bi-x-square"></i></h2>
+              <button class="btn">
+                <h2 class="icon-heading"><i class="bi bi-x-square"></i></h2>
+              </button>
+
             </th>
 
             <!-- Product information  -->
             <td>
 
-            <div class="row">
-            <div class="col-2 product-image m-2 justify-content-center">
-              <img src="https://picsum.photos/200/300" class="product-image" alt="">
-            </div>
-            <div class="col">
+              <div class="row">
+                <div class="col-2 product-image m-2 justify-content-center">
+                  <img src="https://picsum.photos/200/300" class="product-image" alt="">
+                </div>
+                <div class="col">
 
-              <strong>Inventory application built with flutter</strong>
-              <br>
-              By Collins Dauda
-              <br>
-              <h2 class="icon-heading"><i class="bi bi-bookmark-heart"></i></h2>
+                  <strong>Inventory application built with flutter</strong>
+                  <br>
+                  By Collins Dauda
+                  <br>
+                  <h2 class="icon-heading"><i class="bi bi-bookmark-heart"></i></h2>
 
-            </div>
-            </div>
+                </div>
+              </div>
 
 
             </td>
@@ -101,11 +106,117 @@
         </tbody>
       </table>
       <div class="d-grid gap-1 d-md-flex justify-content-md-end">
-              <a href="marketplace.php"><button class="btn btn-primary me-md-2" type="button">Continue shopping</button></a>
-              <a><button class="btn btn-primary" type="button">Complete purchase</button></a>
-            </div>
+        <a href="marketplace.php"><button class="btn btn-primary me-md-2" type="button">Continue shopping</button></a>
+
+        <a class="btn btn-primary" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Proceed to payment</a>
+      </div>
 
     </div>
+
+
+    <!-- checkout modals -->
+    <div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+      <div class="modal-dialog modal-dialog-centered">
+
+      <!-- modal one  -->
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalToggleLabel"> Payment </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form class="row g-3" id="payment_form">
+              <script type="text/javascript">
+                const form = $('#payment_form');
+                form.submit(function(e){
+                  e.preventDefault();
+                })
+              </script>
+            <!-- mobile number  -->
+              <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Mobile number</label>
+                <input type="email" class="form-control" id="inputEmail4">
+              </div>
+
+              <!-- holder name  -->
+              <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Account Holder name</label>
+                <input type="password" class="form-control" id="inputPassword4">
+              </div>
+
+              <!-- mobile money provider  -->
+              <div class="col-md-4">
+                <label for="inputState" class="form-label">Payment Provider</label>
+                <select id="inputState" class="form-select">
+                  <option selected>MTN</option>
+                  <option>Vodafone</option>
+                </select>
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Open second modal</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- payment success modal  -->
+    <div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalToggleLabel2">Success</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            Payment successfully recieved!
+          </div>
+          <div class="modal-footer">
+            <a href="marketplace.php" class="btn btn-success"> Continue shopping</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <script type="text/javascript">
+      var myModal = document.getElementById('myModal')
+      var myInput = document.getElementById('myInput')
+
+      myModal.addEventListener('shown.bs.modal', function() {
+        myInput.focus()
+      })
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
