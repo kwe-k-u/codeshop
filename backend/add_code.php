e815dcea2a8ff612e29b69864c85cd65c74cbae4
Yemofio <95033608+Yemofio@users.noreply.github.com>
2021-11-25 22:18:52 +0000

    Update add_code.php

--- backend/add_code.php
+++ backend/add_code.php
@@ -1,10 +1,7 @@
 <?php 
-require('../Controllers/code_controller.php');
-
-
+require('code_controller.php');
 
 if(isset($_POST['update_prod'])){
-    // retrieve the name, description and quantity from the form submission
     $code_id = $_POST['cd_id'];
     $language = $_POST[''];
     $upload_date = $_POST[date("Y-m-d")];
@@ -12,14 +9,12 @@ if(isset($_POST['update_prod'])){
 
     echo $code_id,$language,$upload_date,$fileloc;
 
-    // call the add_product_controller function: return true or false
     $result =   update_code_controller($code_id,$language,$upload_date,$fileloc);
 
 
     if($result === true){
         header('Location: ../Admin/code_list.php');
         
-        // header('Location: ');
     } 
     else {
         // header('Location: ../View/add_product_page.php');
@@ -30,34 +25,20 @@ if(isset($_POST['update_prod'])){
 }
 
 if(isset($_POST['del_code'])){
-    // retrieve the name, description and quantity from the form submission
     $code_id = $_POST['cd_id'];
 
-
-
-    // call the add_product_controller function: return true or false
     $result =   delete_code_controller($code_id);
 
 
     if($result === true){
         header('Location: ../Admin/code_list.php');
         
-        // header('Location: ');
     } 
     else {
-        // header('Location: ../View/add_product_page.php');
         echo '<script>alert("Unable to insert")</script>';
         
     }
 
 }
-
-
-
-
-
-
-
-
 
 ?>
